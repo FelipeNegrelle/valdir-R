bd480f4b7efc994ec510dad9cbcadd2c6a4e6200
Felipe <felipenegrelleramos@gmail.com>
2022-07-23 18:32:10 -0300

    updating App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -9,18 +9,19 @@ function App() {
     <>
     <Header />
 
-    <section>
-      <img src="../assets/images/valdir_logo_resized.svg" alt="logo do programa em forma de desenho" />
-    <span>
-      <h1>Quem é valdir?</h1>
-      <p>
-      Valdir é um podcast criado por André Young e seus amigos, onde nele você ouve sobre as desaventuranças deles nos temas mais aleatórios possíveis, como por exemplo: Fezes, ET’s, Rato da AIDS, Folclore brasileiro , Transformers (muitos Transformers) e muito mais!
-      </p>
-    </span>
-    </section>
-    <a href='./Ouvir.tsx'>Ouça aqui!</a>
-
-
+    <main>
+        <section className='flex flex-col '>
+          <img src="../assets/images/valdir_logo_resized.svg" alt="logo do programa em forma de desenho"  className='w-50 h-56'/>
+          <span className='flex flex-col w-483 h-430 text-white bg-[rgba(196, 196, 196, 0.2)] rounded-lg px-2 py-10'>
+            <h1 className='font-questrial font-normal text-5xl'>Quem é valdir?</h1>
+            <p>
+            Valdir é um podcast criado por André Young e seus amigos, onde nele você ouve sobre as desaventuranças deles nos temas mais aleatórios possíveis, como por exemplo: Fezes, ET’s, Rato da AIDS, Folclore brasileiro , Transformers (muitos Transformers) e muito mais!
+            </p>
+          </span>
+        </section>
+        <a href='./Ouvir.tsx'>Ouça aqui!</a>
+    </main>
+    
     <Footer />
     </>
 
